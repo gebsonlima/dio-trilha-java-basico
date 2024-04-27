e89f81268aa70445cbb74e1ecf20c09dd3ce116d
GebsonSilva <gebsonlima@hotmail.com>
2024-04-27 14:54:28 -0300

    Finalizado conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,55 @@
+//Importação Classe Scanner
+import java.util.Scanner;
+
+
+//Inicio do código Conta de Banco
 public class ContaTerminal {
+
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        int numero;
+
+        String agencia;
+
+        String nomeCliente;
+
+        double saldo;
+
+
+
+        System.out.println("Por favor, digite o numero da agencia!");
+
+        agencia = sc.next();
+
+
+
+        System.out.println("Por favor, digite o numero da conta!");
+
+        numero = sc.nextInt();
+
+
+
+        System.out.println("Por favor, digite seu nome!");
+
+        nomeCliente = sc.next();
+
+
+
+        System.out.println("Por favor, digite o saldo da conta!");
+
+        saldo = sc.nextDouble();
+
+        //Exibição da conta final criada, mostrando o total de Saldo.
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+
+                + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponivel para saque.");
+
     }
+
 }
